45b321098213b1f4fc4e148a947b7001122ee7c2
AnassLalkha <lalkhaa1@gmail.com>
2018-09-14 09:36:38 +0200

    Modif User (ajout de username...)

--- src/main/java/fr/TAA/ProjetWeekEnd/User.java
+++ src/main/java/fr/TAA/ProjetWeekEnd/User.java
@@ -19,8 +19,34 @@ public class User {
 	}
 
 	private Long id;
+	private String username;
+	private String password;
+	private String email;
+    public String getUsername() {
+		return username;
+	}
 
-    private String name;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String name;
 
     private List<Sport> favoriteSports;
     
