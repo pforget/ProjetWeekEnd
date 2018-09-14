c03aeb86d2d765438be3792cdbd204147431393a
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2018-09-14 16:04:31 +0200

    added the list of deparments attribute

--- src/main/java/fr/TAA/ProjetWeekEnd/Region.java
+++ src/main/java/fr/TAA/ProjetWeekEnd/Region.java
@@ -1,20 +1,27 @@
 package fr.TAA.ProjetWeekEnd;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
 @Entity
 public class Region {	
-
-	public Region(String name) {
-		this.name = name;
-	}
 	
+	public Region(String name, List<Department> departments) {
+		super();
+		this.name = name;
+		this.departments = departments;
+	}
+
 	public Region() {
 		
 	}
 
 	private String name;
+	
+	private List<Department> departments;
 	
 	@Id
 	public String getName() {
@@ -23,6 +30,15 @@ public class Region {
 	
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Transient
+	public List<Department> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(List<Department> departments) {
+		this.departments = departments;
 	}
 	
 }
