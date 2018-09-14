a96f1f524bedca3562cca4718ba52eef6cbe1ae4
prune <prunefd@gmail.com>
2018-09-14 08:26:44 +0200

    City

--- src/main/java/fr/TAA/ProjetWeekEnd/City.java
+++ src/main/java/fr/TAA/ProjetWeekEnd/City.java
@@ -6,12 +6,15 @@ import javax.persistence.Entity;
 @Entity
 public class City {
 	
-	public void City(String name, int PostalCode) {
+	public void City(String name, int PostalCode, Department departmentID) {
 		this.cityPK = new CityPK(name, PostalCode);
+		this.departmentID = departmentID;
 	}
 	
 	@EmbeddedId
 	private CityPK cityPK;
+	
+	private Department departmentID;
 	
 	public void City() {
 		
