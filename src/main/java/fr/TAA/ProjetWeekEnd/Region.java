f5bd661995f8ec39acb683856c150d7ebabb80f2
prune <prunefd@gmail.com>
2018-09-14 08:32:24 +0200

    Region id is name

--- src/main/java/fr/TAA/ProjetWeekEnd/Region.java
+++ src/main/java/fr/TAA/ProjetWeekEnd/Region.java
@@ -1,15 +1,12 @@
 package fr.TAA.ProjetWeekEnd;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 public class Region {	
 
-	public Region(int id, String name) {
-		super();
-		this.id = id;
+	public Region(String name) {
 		this.name = name;
 	}
 	
@@ -17,19 +14,9 @@ public class Region {
 		
 	}
 
-	private int id;
 	private String name;
 	
 	@Id
-	@GeneratedValue
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
 	public String getName() {
 		return name;
 	}
