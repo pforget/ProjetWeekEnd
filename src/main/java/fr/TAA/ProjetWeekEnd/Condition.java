58f5596a4d1ac9099d66e80fdfdfc45502aca957
prune <prunefd@gmail.com>
2018-09-14 14:15:54 +0200

    annotation relation

--- src/main/java/fr/TAA/ProjetWeekEnd/Condition.java
+++ src/main/java/fr/TAA/ProjetWeekEnd/Condition.java
@@ -1,61 +1,13 @@
 package fr.TAA.ProjetWeekEnd;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Condition {
+	
 	private long identifier;
-	public long getIdentifier() {
-		return identifier;
-	}
-	public void setIdentifier(long identifier) {
-		this.identifier = identifier;
-	}
-	public double getMinTemperature() {
-		return minTemperature;
-	}
-	public void setMinTemperature(double minTemperature) {
-		this.minTemperature = minTemperature;
-	}
-	public double getMinPluviometry() {
-		return minPluviometry;
-	}
-	public void setMinPluviometry(double minPluviometry) {
-		this.minPluviometry = minPluviometry;
-	}
-	public double getMinSunshine() {
-		return minSunshine;
-	}
-	public void setMinSunshine(double minSunshine) {
-		this.minSunshine = minSunshine;
-	}
-	public double getMinWindCondition() {
-		return minWindCondition;
-	}
-	public void setMinWindCondition(double minWindCondition) {
-		this.minWindCondition = minWindCondition;
-	}
-	public double getMaxWindCondition() {
-		return maxWindCondition;
-	}
-	public void setMaxWindCondition(double maxWindCondition) {
-		this.maxWindCondition = maxWindCondition;
-	}
-	public double getMaxTemperature() {
-		return maxTemperature;
-	}
-	public void setMaxTemperature(double maxTemperature) {
-		this.maxTemperature = maxTemperature;
-	}
-	public double getMaxPluviometry() {
-		return maxPluviometry;
-	}
-	public void setMaxPluviometry(double maxPluviometry) {
-		this.maxPluviometry = maxPluviometry;
-	}
-	public double getMaxSunshine() {
-		return maxSunshine;
-	}
-	public void setMaxSunshine(double maxSunshine) {
-		this.maxSunshine = maxSunshine;
-	}
 	private double minTemperature;
 	private double minPluviometry;
 	private double minSunshine;
@@ -64,5 +16,71 @@ public class Condition {
 	private double maxTemperature;
 	private double maxPluviometry;
 	private double maxSunshine;
+	
+	@Id
+	public long getIdentifier() {
+		return identifier;
+	}
+	public void setIdentifier(long identifier) {
+		this.identifier = identifier;
+	}
+	
+	public double getMinTemperature() {
+		return minTemperature;
+	}
+	public void setMinTemperature(double minTemperature) {
+		this.minTemperature = minTemperature;
+	}
+	
+	public double getMinPluviometry() {
+		return minPluviometry;
+	}
+	public void setMinPluviometry(double minPluviometry) {
+		this.minPluviometry = minPluviometry;
+	}
+	
+	public double getMinSunshine() {
+		return minSunshine;
+	}
+	public void setMinSunshine(double minSunshine) {
+		this.minSunshine = minSunshine;
+	}
+	
+	public double getMinWindCondition() {
+		return minWindCondition;
+	}
+	public void setMinWindCondition(double minWindCondition) {
+		this.minWindCondition = minWindCondition;
+	}
+	
+	public double getMaxWindCondition() {
+		return maxWindCondition;
+	}
+	public void setMaxWindCondition(double maxWindCondition) {
+		this.maxWindCondition = maxWindCondition;
+	}
+	
+	public double getMaxTemperature() {
+		return maxTemperature;
+	}
+	public void setMaxTemperature(double maxTemperature) {
+		this.maxTemperature = maxTemperature;
+	}
+	
+	public double getMaxPluviometry() {
+		return maxPluviometry;
+	}
+	public void setMaxPluviometry(double maxPluviometry) {
+		this.maxPluviometry = maxPluviometry;
+	}
+	
+	@Column(nullable = false)
+	public double getMaxSunshine() {
+		return maxSunshine;
+	}
+	public void setMaxSunshine(double maxSunshine) {
+		this.maxSunshine = maxSunshine;
+	}
+
 
 }
