5d358ad0f504e191a82d6772b12bf740e9ebe574
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2018-09-17 14:39:12 +0200

    Implementation interface

--- src/main/java/DAO/DepartmentDAO.java
+++ src/main/java/DAO/DepartmentDAO.java
@@ -2,56 +2,77 @@ package DAO;
 
 import org.hibernate.mapping.Map;
 
-import fr.TAA.ProjetWeekEnd.EntityManagerHelper;
+import fr.TAA.ProjetWeekEnd.City;
+import fr.TAA.ProjetWeekEnd.Department;
 
 import java.util.List;
+
+import javax.persistence.EntityManager;
 
 
 public class DepartmentDAO  implements AbstractDAO {
 
 	public DepartmentDAO() {
-		super();
-		// TODO Auto-generated constructor stub
+
 	}
-	EntityManagerHelper manager;
-	public DepartmentDAO(EntityManagerHelper manager) {
+	
+	EntityManager manager;
+	public DepartmentDAO(EntityManager manager) {
 		this.manager = manager;
 	}
+	
 	public long countAll(Map param) {
-		// TODO Auto-generated method stub
-		return 0;
+		String query = "select count(d) from Department as d";
+		return (Long) manager.createQuery(query).getSingleResult();
 	}
 
 	public Object findByID(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select d from Department as d where d.id = :id";
+		return manager.createQuery(query).setParameter("id", id).getSingleResult();
 	}
 
 	public List<Object> findByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select d from Department as d where d.name = :name";
+		return manager.createQuery(query).setParameter("name", name).getResultList();
 	}
 
 	public Boolean exist(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Department d = (Department) o;
+		return findByID(d.getId()) != null;
 	}
 
 	public List<Object> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select d from Department as d";
+		return manager.createQuery(query).getResultList();
 	}
+	
 	public Boolean add(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Department d = (Department) o;
+		manager.getTransaction().begin();
+		manager.persist(d);
+		manager.getTransaction().commit();
+		return true;
 	}
+	
 	public Boolean delete(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Department d = (Department) o;
+		manager.getTransaction().begin();
+		manager.remove(d);
+		manager.getTransaction().commit();
+		return true;
 	}
+	
 	public Boolean update(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Department d = (Department) o;
+		Department dOutOfDate = (Department) findByID(d.getId());
+		
+		manager.getTransaction().begin();
+		dOutOfDate.setName(d.getName());
+		dOutOfDate.setCities(d.getCities());
+		dOutOfDate.setRegionId(d.getRegionId());
+		manager.getTransaction().commit();
+		
+		return true;
 	}
 
 }
