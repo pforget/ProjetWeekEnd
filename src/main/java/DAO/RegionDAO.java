6e6af0891048dc6bdafb3a00531d1400bf02e2a4
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2018-09-17 14:40:19 +0200

    Implementation interface

--- src/main/java/DAO/RegionDAO.java
+++ src/main/java/DAO/RegionDAO.java
@@ -2,28 +2,31 @@ package DAO;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import org.hibernate.mapping.Map;
 
-import fr.TAA.ProjetWeekEnd.EntityManagerHelper;
+import fr.TAA.ProjetWeekEnd.Department;
+import fr.TAA.ProjetWeekEnd.Region;
 
 public class RegionDAO  implements AbstractDAO {
 
 	public RegionDAO() {
-		super();
-		// TODO Auto-generated constructor stub
+
 	}
-	EntityManagerHelper manager;
-	public RegionDAO(EntityManagerHelper manager) {
+	
+	EntityManager manager;
+	public RegionDAO(EntityManager manager) {
 		this.manager = manager;
 	}
 	public long countAll(Map param) {
-		// TODO Auto-generated method stub
-		return 0;
+		String query = "select count(r) from Region as r";
+		return (Long) manager.createQuery(query).getSingleResult();
 	}
 
 	public List<Object> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select r from Region as r";
+		return manager.createQuery(query).getResultList();
 	}
 
 	public Object findByID(long id) {
@@ -32,25 +35,37 @@ public class RegionDAO  implements AbstractDAO {
 	}
 
 	public List<Object> findByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select r from Region as r where r.name = :name";
+		return manager.createQuery(query).setParameter("name", name).getResultList();
 	}
 
 	public Boolean exist(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Region r = (Region) o;
+		return findByName(r.getName()) != null;
 	}
 	public Boolean add(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Region r = (Region) o;
+		manager.getTransaction().begin();
+		manager.persist(r);
+		manager.getTransaction().commit();
+		return true;
 	}
 	public Boolean delete(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Region r = (Region) o;
+		manager.getTransaction().begin();
+		manager.remove(r);
+		manager.getTransaction().commit();
+		return true;
 	}
 	public Boolean update(Object o) {
-		// TODO Auto-generated method stub
-		return null;
+		Region r = (Region) o;
+		Region rOutOfDate = (Region) findByName(r.getName());
+		
+		manager.getTransaction().begin();
+		rOutOfDate.setDepartments(r.getDepartments());;
+		manager.getTransaction().commit();
+		
+		return true;
 	}
 
 }
