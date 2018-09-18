f533b3fbfb56f1f40ba9018d522307e4d4868e0b
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2018-09-18 15:32:23 +0200

    Correction findByID

--- src/main/java/DAO/RegionDAO.java
+++ src/main/java/DAO/RegionDAO.java
@@ -13,11 +13,12 @@ public class RegionDAO {
 	public RegionDAO() {
 
 	}
-	
+
 	EntityManager manager;
 	public RegionDAO(EntityManager manager) {
 		this.manager = manager;
 	}
+	
 	public long countAll(Map param) {
 		String query = "select count(r) from Region as r";
 		return (Long) manager.createQuery(query).getSingleResult();
@@ -28,34 +29,37 @@ public class RegionDAO {
 		return manager.createQuery(query).getResultList();
 	}
 
-	public Object findByID(String id) {		
+	public Region findByID(String id) {		
 		String query = "select r from Region as r where r.name = :name";
-		return manager.createQuery(query).setParameter("name", id).getFirstResult();
+		return (Region) manager.createQuery(query).setParameter("name", id).getSingleResult();
 	}
 
 
 	public Boolean exist(Region r) {
 		return findByID(r.getName()) != null;
 	}
+
 	public Boolean add(Region r) {
 		manager.getTransaction().begin();
 		manager.persist(r);
 		manager.getTransaction().commit();
 		return true;
 	}
+
 	public Boolean delete(Region r) {
 		manager.getTransaction().begin();
 		manager.remove(r);
 		manager.getTransaction().commit();
 		return true;
 	}
+
 	public Boolean update(Region r) {
 		Region rOutOfDate = (Region) findByID(r.getName());
-		
+
 		manager.getTransaction().begin();
 		rOutOfDate.setDepartments(r.getDepartments());;
 		manager.getTransaction().commit();
-		
+
 		return true;
 	}
 
