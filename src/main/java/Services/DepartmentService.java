69c1c1d99b7a5f0f4d230e407267f507781578ed
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2018-09-18 10:40:41 +0200

    Squelettes du reste des services

---
+++ src/main/java/Services/DepartmentService.java
@@ -0,0 +1,32 @@
+package Services;
+
+import java.util.List;
+
+public class DepartmentService implements InterfaceService {
+
+	public Boolean create(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean update(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object findById(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Object> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
