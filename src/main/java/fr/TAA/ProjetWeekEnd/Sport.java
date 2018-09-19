7c0d55d5cf26df7e5605a51b19ed2196bce292ad
KevPB <kevin.paratre-badois@etudiant.univ-rennes1.fr>
2018-09-19 14:34:31 +0200

    Le retour du Sport

---
+++ src/main/java/fr/TAA/ProjetWeekEnd/Sport.java
@@ -0,0 +1,79 @@
+package fr.TAA.ProjetWeekEnd;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+
+enum level {Begginer,Advanced,Expert};
+
+@Entity
+public class Sport {
+    
+    @EmbeddedId
+    private SportPK sportPK;
+    
+//    private boolean needwater;
+//    private boolean needmountain;
+    private Condition WeatherCondition;
+    
+    public Sport() {
+        
+    }
+    
+    public Sport(String name, level level, boolean needwater, boolean needmountain, Condition weatherCondition) {
+        this.sportPK = new SportPK(name, level);
+//        this.needwater = needwater;
+//        this.needmountain = needmountain;
+        WeatherCondition = weatherCondition;
+    }
+    
+    public SportPK getSportPK() {
+        return sportPK;
+    }
+
+    public void setSportPK(SportPK sportPK) {
+        this.sportPK = sportPK;
+    }
+
+    public String getName() {
+        return sportPK.getName();
+    }
+
+    public void setName(String name) {
+        sportPK.setName(name);
+    }
+/*
+    public boolean isNeedwater() {
+        return needwater;
+    }
+
+    public void setNeedwater(boolean needwater) {
+        this.needwater = needwater;
+    }
+
+    public boolean isNeedmountain() {
+        return needmountain;
+    }
+
+    public void setNeedmountain(boolean needmountain) {
+        this.needmountain = needmountain;
+    }
+*/
+    @ManyToOne
+    public Condition getWeatherCondition() {
+        return WeatherCondition;
+    }
+
+    public void setWeatherCondition(Condition weatherCondition) {
+        WeatherCondition = weatherCondition;
+    }
+    
+    public boolean isPracticable(Weather weather) {
+        return WeatherCondition.isFulfilled(weather);
+    }
+    
+    
+    
+
+}
